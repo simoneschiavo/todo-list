6e1a84553b9459ce7c6ccb5444cb6fa9719e20fc
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-12 16:51:52 +0100

    Add Project class

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,28 @@
 import { TaskManager } from "./tasks";
+
+export class Project {
+    constructor(name) {
+        this.name = name;
+        this.taskManager = new TaskManager();
+    }
+
+    getAllTasks() {
+        return this.taskManager.getAllTasks();
+    }
+
+    getTask(index) {
+        return this.taskManager.getTask(index);
+    }
+
+    createTask(title, description, dueDate, priority) {
+        return this.taskManager.createTask(title, description, dueDate, priority);
+    }
+
+    updateTask(index, updatedData) {
+        return this.taskManager.updateTask(index, updatedData);
+    }
+
+    deleteTask(index) {
+        return this.taskManager.deleteTask(index);
+    }
+}
