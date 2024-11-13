77aeefe46f2591e97e1a615e69cf3ffd69ad7143
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-13 07:55:11 +0100

    Add the ProjectManager class
    Performs CRUD operations on the Project class

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,9 @@
 import { TaskManager } from "./tasks";
 
 export class Project {
-    constructor(name) {
+    constructor(name, description) {
         this.name = name;
+        this.description = description;
         this.taskManager = new TaskManager();
     }
 
@@ -24,5 +25,41 @@ export class Project {
 
     deleteTask(index) {
         return this.taskManager.deleteTask(index);
+    }
+}
+
+export class ProjectManager {
+    constructor() {
+        this.projects = [];
+    }
+
+    createProject(name, description) {
+        const project = new Project(name, description);
+        this.projects.push(project);
+        return project;
+    } 
+
+    getProject(index) {
+        return this.projects[index];
+    }
+
+    getAllProjects() {
+        return this.projects;
+    }
+
+    updateProject(index, updatedData) {
+        const project = this.projects[index];
+        if (project) {
+            project.name = updatedData.name || project.name;
+            project.description = updatedData.description || project.description;
+        }
+    }
+
+    deleteProject(index) {
+        if (index >= 0 && index < this.projects.length) {
+            this.projects.splice(index, 1);
+            return true;
+        }
+        return false;
     }
 }
