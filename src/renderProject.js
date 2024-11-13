406a55ce2c96f8e8afcbb2c6518606c62ebaecea
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-13 13:38:23 +0100

    Add renderDefaultProject function

---
+++ src/renderProject.js
@@ -0,0 +1,21 @@
+import defaultProjectIcon from "./img/default-project-icon.svg";
+
+function renderDefaultProject(project) {
+    const defaultProjectsContainer = document.querySelector("nav > .default");
+
+    const projectDiv = document.createElement("div");
+    projectDiv.classList.add("project");
+
+    const icon = document.createElement("img");
+    icon.src = project.icon || defaultProjectIcon;
+    icon.alt = `${project.name} icon`;
+
+    const title = document.createElement("h3");
+    title.innerText = project.name;
+
+    projectDiv.append(icon, title);
+
+    defaultProjectsContainer.appendChild(projectDiv);
+}
+
+export { renderDefaultProject };
