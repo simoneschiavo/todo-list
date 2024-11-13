8219621622ff5c23b9a465663aa671e73284a137
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-13 15:34:08 +0100

    Add more default projects

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import "./styles.css";
 import { ProjectManager } from "./projects";
 import { renderDefaultProject, renderCustomProject } from "./domManager";
 import homeIcon from "./img/home-icon.svg";
+import todayIcon from "./img/today-icon.svg";
+import somedayIcon from "./img/someday-icon.svg";
 
 const projectManager = new ProjectManager();
 
-const homeProject = projectManager.createProject("Home", "test", homeIcon);
+const homeProject = projectManager.createProject("Home", "", homeIcon);
+const todayProject = projectManager.createProject("Today", "", todayIcon);
+const somedayProject = projectManager.createProject("Someday", "", somedayIcon);
+
 renderCustomProject(homeProject);
+renderCustomProject(todayProject);
+renderCustomProject(somedayProject);
