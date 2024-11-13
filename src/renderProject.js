64bd8712be7f1a37ce6aca24063ccf7674202923
Simone Schiavo <simoneschiavo@icloud.com>
2024-11-13 14:06:46 +0100

    Add renderCustomProject function

--- src/renderProject.js
+++ src/renderProject.js
@@ -18,4 +18,22 @@ function renderDefaultProject(project) {
     defaultProjectsContainer.appendChild(projectDiv);
 }
 
-export { renderDefaultProject };
+function renderCustomProject(project) {
+    const customProjectsContainer = document.querySelector("nav > .custom");
+
+    const projectDiv = document.createElement("div");
+    projectDiv.classList.add("project");
+
+    const icon = document.createElement("img");
+    icon.src = project.icon || defaultProjectIcon;
+    icon.alt = `${project.name} icon`;
+
+    const title = document.createElement("h3");
+    title.innerText = project.name;
+
+    projectDiv.append(icon, title);
+
+    customProjectsContainer.appendChild(projectDiv);
+}
+
+export { renderDefaultProject, renderCustomProject };
